fca0ad9c07db2bbbd41c7825667185f2923be54f
SanjuPeter06051980 <118700864+SanjuPeter06051980@users.noreply.github.com>
2023-03-05 17:41:21 +0530

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,120 @@
+function def(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Sound is a form of energy that produces the sensation of hearing in our ears. Sound is produced by vibrations.  Sound is caused when a body vibrates. sound can be produced by a tuning fork due to vibrations of its arms";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function tlw(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "A Transverse Wave is the wave in which the particles of the medium vibrate about their mean positions, in a direction that is perpendicular to the direction of wave propagation. Longitudinal waves are waves in which the vibration of the medium is parallel to the direction the wave travels and displacement of the medium is in the same direction of the wave propagation. Longitudinal waves are made of compressions and rarefactions, while transverse waves are made of crests and troughs";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function med(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Sound needs a material medium for their propagation like solid, liquid or gas to travel because the molecules of solid, liquid and gases carry sound waves from one point to another. Sound cannot progress through the vacuum because the vacuum has no molecules which can vibrate and carry the sound waves.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function cora(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Compression is a region in a longitudinal wave where the particles are closest together. A rarefaction is a region in a longitudinal wave where the particles are furthest apart. The region where the medium is compressed is known as compression and the region where the medium is spread out is known as a rarefaction.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function amp(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The amplitude of a sound wave is the measure of the height of the wave. The amplitude of a sound wave can be defined as the loudness or the amount of maximum displacement of vibrating particles of the medium from their mean position when the sound is produced. It is the distance between the crest or trough and the mean position of the wave. Loudness is directly proportional to the amplitude of the sound. If the amplitude of a sound wave is large, then the loudness of the sound will be more. If the amplitude is small, then the sound will be feeble.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function fre(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The number of oscillations per second is known as the frequency of oscillation. Its unit is hertz and is denoted by Hz. The frequency of a wave in general means how frequently the particles of a medium vibrate when a wave moves through the medium.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function tp(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The time taken by the particle to complete one vibration cycle is the time period for that particle. It is measured in seconds";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function wl(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Wavelength of a sound wave can be defined as the distance between the two successive crests or two successive troughs of a  transverse wave or two successive compressions and rarefactions in a longitudinal wave. Wavelength is the distance/length of waves. It is measured in metres and expressed using lamda";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function wv(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Wave velocity can be defined as the distance travelled by a wave in one second. It is also known as wave speed.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+
+function eq(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Frequency is found to be equal to one by the time period of the wave. Wavelength is foud to be equal to the wave velocity divided by the frequency";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function speed(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The speed of sound varies from substance to substance: typically, sound travels most slowly in gases, faster in liquids, and fastest in solids. For example, while sound travels at 343 metres per second in air, it travels at 1,481 metres per second in water and at 5,120 metres per second in iron. The speed of sound in air varies. It increases by 0.61 metres per second for each 1°C rise in temperature.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function tl(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The speed of sound is far less than the speed of light  in air. Thus sound of thunder will take more time to reach the observer compared to light. Hence, a flash of lightning is seen before we hear thunder.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+
+function ar(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Audible range of frequency is the range of frequency within which the sound can be heard by a human being, that is twenty to twenty thousand Hertz";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function infra(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "The sound waves, having frequencies below twenty Hertz are called infrasonic sounds. Human beings can not hear an infrasonic sound.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function ultra(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Sound waves with frequencies higher than the upper audible limit of human hearing are called ultrasound. The limit varies from person to person but is approximately twenty thousand Hertz. The physical properties of ultrasound are similar to the normal audible sound. This type of scientific concept is used in many different fields such as navigation, medicine, imaging, cleaning, mixing, communication, testing and so on. ";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function ss(){
+    var synth=window.speechSynthesis;                                                                                                                                                                                                                                                                                                                                                           
+    speak_data= "Supersonic is used for objects which travel at a speed greater than the speed of sound.";
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function Logout(){
+    window.location ="experimet.html";
+}
+
+
+
